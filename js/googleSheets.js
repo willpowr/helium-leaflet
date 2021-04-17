cd1b97ed6761757197e0c2d99de1e4ecdd00235f
willpowr <hwillj@gmail.com>
2021-04-17 15:16:52 +0100

    changed to module

--- js/googleSheets.js
+++ js/googleSheets.js
@@ -1,9 +1,9 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
+import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 const doc = new GoogleSpreadsheet('1IoIabHVGONxSG2mlip7CiYi0MS8KGe4F2dW1OnjfqPg');
 
 // https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication
-const creds = require('../config/secret/helium-311010-cd25337b10cf.json'); // the file saved above
+import creds from '../config/secret/helium-311010-cd25337b10cf.json'; // the file saved above
 
 const authenticate = async () => {
     await doc.useServiceAccountAuth(creds);
